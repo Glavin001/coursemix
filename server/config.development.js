b94667daa95b6328fc4adaafb486fa5682b7679c
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-03-21 20:22:08 -0300

    Add MongoDB Bluemix service config

--- server/config.development.js
+++ server/config.development.js
@@ -4,12 +4,6 @@
 var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 // TODO: Get application information and use it in your app.
 
-// VCAP_SERVICES contains all the credentials of services bound to
-// this application. For details of its content, please refer to
-// the document or sample of each service.
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-// TODO: Get service credentials and communicate with bluemix services.
-
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 // The port on the DEA for communication with the application:
---
+++ server/datasources.development.js
@@ -0,0 +1,45 @@
+var mongoUrl = "mongodb://localhost";
+
+if (process.env.VCAP_SERVICES) {
+  // VCAP_SERVICES contains all the credentials of services bound to
+  // this application. For details of its content, please refer to
+  // the document or sample of each service.
+  var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+  // TODO: Get service credentials and communicate with bluemix services.
+
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  if (env['mongodb-2.4']) {
+    var mongo = env['mongodb-2.4'][0]['credentials'];
+  }
+
+  mongoUrl = services['mongodb-2.4'].credentials.url;
+
+}
+
+/**
+"mongodb-2.4": [
+      {
+         "name": "mongodb-fg",
+         "label": "mongodb-2.4",
+         "plan": "100",
+         "credentials": {
+            "hostname": "75.126.37.68",
+            "host": "75.126.37.68",
+            "port": 10033,
+            "username": "ee376cef-56e7-4669-a499-a94283e529f6",
+            "password": "6d634437-7c8c-453f-8d49-2b22b20b0187",
+            "name": "98afd66b-645b-47c4-9a3d-6db2d28a89ea",
+            "db": "db",
+            "url": "mongodb://ee376cef-56e7-4669-a499-a94283e529f6:6d634437-7c8c-453f-8d49-2b22b20b0187@75.126.37.68:10033/db"
+         }
+      }
+   ]
+*/
+
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "mongodb",
+    "url": mongoUrl
+  }
+};
