ba90ba67ee49e38f43d816b4e23279693765b1b2
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-03-22 11:26:55 -0300

    Add Email (Sendgrid) support for emailing users their new courses

--- common/models/course.js
+++ common/models/course.js
@@ -191,4 +191,110 @@ module.exports = function(Course) {
         }
     );
 
+    Course.emailCourses = function(email, courses, cb) {
+        // console.log(email, courses);
+
+        var text = "Hi there,\n" +
+            "You have " + courses.length + " courses. Here they are:\n" +
+            courses.map(function(course, idx) {
+                return "" + (idx + 1) + ". " + course.name
+            }).join('\n') +
+            "\n" +
+            "Copy and paste this link into your web browser: " +
+            "http://app-coursemix.mybluemix.net/" +
+            "\n\n" +
+            "Thank you for using Coursemix!\n\n" +
+            "Coursemix Team\n";
+
+        var html = "Hi there,<br/><br/>" +
+            "You have " + courses.length +
+            " courses. Here they are:<br/>" +
+            courses.map(function(course, idx) {
+                var meta = JSON.parse(course.app_data.course);
+                var days = [];
+                if (meta.on_monday) {
+                    days.push('Mondays');
+                }
+                if (meta.on_tuesday) {
+                    days.push('Tuesdays');
+                }
+                if (meta.on_wednesday) {
+                    days.push('Wednesdays');
+                }
+                if (meta.on_thursday) {
+                    days.push('Thursdays');
+                }
+                if (meta.on_fridays) {
+                    days.push('Fridays');
+                }
+                if (meta.on_saturdays) {
+                    days.push('Saturdays');
+                }
+                if (meta.on_sundays) {
+                    days.push('Sundays');
+                }
+
+                return ("" + (idx + 1) + ". " + course.name +
+                    " with " +
+                    meta.instructor +
+                    ((days.length > 0) ? (" on " +
+                        days.join(' and ')) : "") +
+                    (meta.begin_time ? (" from " + meta.begin_time
+                            .hours +
+                            ":" + ("00" + meta.begin_time.minutes)
+                            .substr(-2, 2) + " to " + meta.end_time
+                            .hours +
+                            ":" + ("00" + meta.end_time.minutes)
+                            .substr(-2, 2)):"") +
+                        (meta.Bldg_code ? (" at " + meta.Bldg_code) :
+                            ""));
+            }).join('<br/>') +
+            "<br/><br/>" +
+            "Here is a link to <a href=\"http://app-coursemix.mybluemix.net/\">Coursemix</a>." +
+            "<br/><br/>" +
+            "<em>Thank you for using Coursemix!</em><br/><br/>" +
+            "<strong>Coursemix Team</strong><br/>";
+
+        // console.log(text, html);
+
+        Course.app.models.Email.send({
+                to: email,
+                from: "coursemix@coursemix.io",
+                subject: "Here is your desired mix of courses!",
+                text: text,
+                html: html
+            },
+            function(err, result) {
+                cb(err, result);
+                if (err) {
+                    console.log('Upppss something crash', err);
+                    return;
+                }
+                console.log(result);
+            });
+
+    };
+
+
+    Course.remoteMethod(
+        'emailCourses', {
+            http: {
+                path: '/email-courses',
+                verb: 'post'
+            },
+            accepts: [{
+                arg: 'email',
+                type: 'string'
+
+            }, {
+                arg: 'courses',
+                type: 'array'
+            }],
+            returns: {
+                root: true,
+                type: 'array'
+            }
+        }
+    );
+
 };
